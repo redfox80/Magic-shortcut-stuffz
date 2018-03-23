a4ec2cb64c56240e1dea83ecc6afbd41b8dc767b
redfox80 <joafor2@gmail.com>
2018-03-23 05:25:33 +0100

    Added gulp-sass with watch

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Magic-shortcut-stuffz
+
+Random giberish to be copy pasta'ed to avoid reinventing the wheel every time you want to do something
+
+Have a look arround, i'm not sure what is in here
+
+\- Redfox80
---
+++ simple gulp watch for sass/gulpfile.js
@@ -0,0 +1,41 @@
+/**
+ * Remember to have installed gulp globally with
+ * npm i gulp -g
+ * 
+ * Also install gulp and gulp-sass locally with
+ * npm i gulp --save-dev
+ * npm i gulp-sass --save-dev
+ */
+
+//------------------------------
+//MUST CONFIGURE THESE VARIABLES
+//------------------------------
+
+
+//Rellative of absolute path to sass file
+var src = "./assets/sass/master.sass";
+
+//Rellative of absolute path to output destination
+var dest = "./redfox80.github.io/css";
+
+
+//------------------------------
+//DON'T GIVE A SHIT ABOUT ANYTHING BELLOW THIS LINE *It's magic*
+//------------------------------
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('watch', function() {
+    
+    gulp.watch(src, ['sass']);
+
+});
+
+gulp.task('sass', function() {
+
+    return gulp.src(src)
+        .pipe(sass())
+        .pipe(gulp.dest(dest));
+
+});
